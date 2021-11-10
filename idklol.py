616f26b1e35d51cf608304bb2276a854ce83266c
tanishraj121 <90378359+tanishraj121@users.noreply.github.com>
2021-11-10 10:49:40 +0530

    Create idklol.py

---
+++ idklol.py
@@ -0,0 +1,98 @@
+score = 0
+
+print('Hi!, would you like to play our guessing game?')
+input1 = input()
+if input1 == 'yes':
+    print('Ok lets get started!')
+    Question = input("is pear a fruit or vegetable:")
+if Question == "fruit":
+	print ("well done")       
+elif Question == ("vegetable"):
+	print ("aww, it was wrong try again next time.")
+
+yooo = input("is apple a fruit or vegetable:")
+if yooo == ("fruit"):
+	print ("well done")
+elif yooo == ("vegetable"):
+	print ("aww, it was wrong try again next time.")
+
+a = input("is onion a fruit or vegetable:")
+if a == ("vegetable"):
+	print ("well done")
+elif a == ("fruit"):
+	print ("aww, it was wrong try again next time.")
+
+b = input("is orange a fruit or vegetable:")
+if b == ("fruit"):
+	print ("well done")
+elif b == ("vegetable"):
+	print ("aww, it was wrong try again next time.")
+
+c = input("is cabbage a fruit or vegetable:")
+if c == ("vegetable"):
+	print ("well done")
+elif c == ("fruit"):
+	print ("aww, it was wrong try again next time.")
+
+d = input("is eggplant a fruit or vegetable:")
+if d == ("vegetable"):
+	print ("well done")
+elif d == ("fruit"):
+	print ("aww, it was wrong try again next time.")
+
+e = input("is spinach a fruit or vegetable:")
+if e == ("vegetable"):
+	print ("well done")
+elif e == ("fruit"):
+	print ("aww, it was wrong try again next time.")
+
+f = input("is papaya a fruit or vegetable:")
+if f == ("fruit"):
+	print ("well done")
+elif f == ("vegetable"):
+	print ("aww, it was wrong try again next time.")
+
+g = input("is broccoli a fruit or vegetable:")
+if g == ("vegetable"):
+	print ("well done")
+elif g == ("fruit"):
+	print ("aww, it was wrong try again next time.")
+
+h = input("is onion a fruit or vegetable:")
+if h == ("vegetable"):
+	print ("well done")
+elif h == ("fruit"):
+	print ("aww, it was wrong try again next time.")
+
+i = input("is banana a fruit or vegetable:")
+if i == ("fruit"):
+	print ("well done")
+elif i == ("vegetable"):
+	print ("aww, it was wrong try again next time.")
+
+j = input("is tomato a fruit or vegetable:")
+if j == ("fruit"):
+	print ("well done")
+elif j == ("vegetable"):
+	print ("aww, it was wrong try again next time.")
+
+k = input("is peach a fruit or vegetable:")
+if k == ("fruit"):
+	print ("well done")
+elif k == ("vegetable"):
+	print ("aww, it was wrong try again next time.")
+
+answer = input("is potato a fruit or vegetable:")
+if answer == ("vegetable"):
+	print ("well done")
+elif answer == ("fruit"):
+	print ("aww, it was wrong try again next time.")
+   print('you scored',score,'points!')
+    
+elif input1 == 'no':
+    print('Thank you for using our program!')
+else:
+    print('incorrect statement.')
+
+    
+    
